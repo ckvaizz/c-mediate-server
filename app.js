e33f96e6a21c4a3e5cf6e5be9f487d329232f641
ckvaizz <vaisakh.k591@gmail.com>
2022-01-05 19:51:12 +0530

    added DB and .env

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongodb = require('./config/mongoConnection')
+require('dotenv').config()
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+mongodb.connect()
 var app = express();
 
 app.use(logger('dev'));
@@ -14,7 +17,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    res.status(404).json({err:'not found'})
+  });
+
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    console.log(err);
+    res.send('error');
+  });
 
 module.exports = app;
